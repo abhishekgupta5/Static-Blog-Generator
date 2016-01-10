9f6e0411c3df05e38cb33708af4f9be65bd46f1f
abhishekgupta5 <abhishekmodi2011@gmail.com>
2016-01-10 17:28:59 +0530

    Added 'convert_all_md' method

--- blog_generator.py
+++ blog_generator.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+
 from markdown2 import Markdown
+from os import listdir
 
 #Static Blog Generator
 def generate_post(source, destination):
@@ -12,3 +14,17 @@ def generate_post(source, destination):
 
     fobj_read.close()
     fobj_write.close()
+
+def convert_all_md(source, destination):
+
+    markdown_object = Markdown()
+    i = 1
+    for fil in listdir(source):
+        if fil.endswith('.md'):
+            fobj_read = open(source+fil)
+            html_converted = markdown_object.convert(fobj_read.read())
+            fobj_write = open(destination+"markdown_converted_%d.html" % i , 'w')
+            fobj_write.write(html_converted)
+            i+=1
+            fobj_read.close()
+            fobj_write.close()
